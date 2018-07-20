3e7aee3a62d071702360f50bf943a7304085a15f
Annette Graham <annettegraham56@gmail.com>
2018-07-20 16:15:51 +0100

    not quite finished mod5

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component {
 
+  handleClick = (props) => {
+    this.props.clickHandler(this.props.button_text)
+  }
+
   render() {
     return (
-      <li><button>
+      <li><button onClick={this.handleClick.bind(this)}>
         {this.props.button_text}
       </button></li>
     )
